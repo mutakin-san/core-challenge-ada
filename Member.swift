7c96216edc9a3ed07f6d0558ab705f19eb80a660
kambingcode <frewin.saputra@gmail.com>
2025-03-26 13:03:10 +0700

    membuat halama member

---
+++ Member.swift
@@ -0,0 +1,18 @@
+//
+//  Member.swift
+//  CleanPlot
+//
+//  Created by Frewin Saputra on 26/03/25.
+//
+
+import SwiftUI
+
+struct Member: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Member()
+}
