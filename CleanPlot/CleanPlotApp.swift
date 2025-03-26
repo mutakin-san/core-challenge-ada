7349b3055683abd6b7d941a4e722329d1fd858ec
Mutakin <mutakin.email@gmail.com>
2025-03-26 10:11:24 +0700

    Initial Commit

---
+++ CleanPlot/CleanPlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanPlotApp.swift
+//  CleanPlot
+//
+//  Created by Mutakin on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanPlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
