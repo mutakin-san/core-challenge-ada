baa8770d0df33100e4ab206e0add7037f6c177ac
kambingcode <frewin.saputra@gmail.com>
2025-03-27 14:14:03 +0700

    refactor detail history ui

--- CleanPlot/DetailHistory.swift
+++ CleanPlot/DetailHistory.swift
@@ -7,35 +7,102 @@
 
 import SwiftUI
 
+struct CardItem : Identifiable{
+    var id = UUID()
+    var pp : String
+    var nama : String
+    var area : String
+}
+
 struct DetailHistory: View {
+    let items: [CardItem] = [
+        CardItem(pp: "FotoProfil", nama: "Endang", area: "SML"),
+        CardItem(pp: "FotoProfil", nama: "Endang", area: "SML"),
+        CardItem(pp: "FotoProfil", nama: "Endang", area: "SML"),
+        CardItem(pp: "FotoProfil", nama: "Endang", area: "SML"),
+        CardItem(pp: "FotoProfil", nama: "Endang", area: "SML"),
+        CardItem(pp: "FotoProfil", nama: "Endang", area: "SML"),
+        CardItem(pp: "FotoProfil", nama: "Endang", area: "SML"),
+        CardItem(pp: "FotoProfil", nama: "Endang", area: "SML"),
+        CardItem(pp: "FotoProfil", nama: "Endang", area: "SML"),
+        CardItem(pp: "FotoProfil", nama: "Endang", area: "SML"),
+    ]
+    
+    
+    
     var body: some View {
         NavigationView{
-            VStack {
-                HStack{
-                    Text("17 March - 30 March 2025")
-                        .font(.headline)
-                        .padding(.leading)
-                    Spacer()
-                    ShareLink(item: "Jadwal 17 Maret - 30 Maret 2025.pdf") {
-                        Image(systemName:"square.and.arrow.up")
-                            .foregroundColor(.blue)
-                            .padding(.trailing)
-                            .imageScale(.large)
-                    }
+            ScrollView {
+                VStack {
+                    Section(header: Text("Shift Pagi 06.00 - 15.00 WIB")
+                        .font(.system(size: 12))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding([.leading, .top])){
+                            ForEach(items) {item in
+                                ListMemberDetailHistory(item: item)
+                            }
+                        }
+                    Section(header: Text("Shift Siang 08.00 - 17.00 WIB")
+                        .font(.system(size: 12))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding([.leading, .top])){
+                            ForEach(items) {item in
+                                ListMemberDetailHistory(item: item)
+                            }
+                        }
+                    
+                    
                 }
-                Divider()
-                
-                ListMemberDetailHistory()
-                
-                
-                           
             }
-        }.navigationTitle("tes")
+            
+            
+            
+            
+            
+        }.navigationTitle(Text("17 March - 30 March"))
         
         
-    }
+        
+    }//var body
 }
 
 #Preview {
     DetailHistory()
 }
+
+struct ListMemberDetailHistory: View {
+    
+    let item: CardItem
+    
+    var body: some View {
+                
+                GroupBox{
+                    HStack{
+                        //pp
+                        Image(item.pp)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width:50, height:50)
+                            .clipShape(Circle())
+                            .shadow(radius:2)
+                        
+                        //nama & no.hp
+                        HStack{
+                            Text(item.nama)
+                                .padding(.leading, 16)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Spacer()
+                            Text(item.area)
+                                .padding(.trailing)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
+                .padding([.leading, .trailing])
+    }
+    
+}
+
+
