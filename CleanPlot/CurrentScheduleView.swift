612c30fec627174b7a6fccb7133f2ad74a31bab3
mutakin-san <mutakin.email@gmail.com>
2025-04-09 15:34:56 +0700

    add edit area functionality to the current schedule

---
+++ CleanPlot/CurrentScheduleView.swift
@@ -0,0 +1,131 @@
+//
+//  CurrentScheduleView.swift
+//  CleanPlot
+//
+//  Created by Mutakin on 09/04/25.
+//
+
+
+import SwiftUI
+import SwiftData
+
+struct CurrentScheduleView: View {
+    let currentSchedule: Schedule?
+    let areas: [String]
+    
+    @State private var selection = "SML"
+    @State var editMode = false
+    @Environment(\.modelContext) var modelContext
+
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            
+            Text("Halo, Subur!")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading)
+                .padding(.top)
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Text("Buat Jadwal Tim Anda Sekarang!")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading)
+                .font(.subheadline)
+            
+            
+            if(currentSchedule == nil) {
+                VStack(alignment: .center) {
+                    Spacer()
+                    Text("Tidak ada jadwal")
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+                
+            } else {
+                HStack {
+                    Text(currentSchedule?.scheduleId ?? "Unknown")
+                    
+                    Spacer()
+                    
+                    Button {
+                        editMode = !editMode
+                    } label: {
+                        Text(editMode ? "Done" : "Edit")
+                    }
+                    
+                    
+                    ShareLink(
+                        item: """
+\(currentSchedule?.scheduleId ?? "Unkonwn")
+\(currentSchedule?.getAssignmentsText() ?? "Tidak ada data")
+""")
+                    .labelStyle(.iconOnly)
+                }
+                .padding()
+                
+                
+                List(currentSchedule?.sortByShift() ?? []) { assignment in
+                    
+                    HStack {
+                        Image(.profilePicture)
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                            .scaledToFill()
+                            .clipShape(.circle)
+                        
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text(assignment.member.name)
+                                
+                                Spacer()
+                                
+                                if editMode {
+                                    
+                                    Picker("Area", selection: Binding(
+                                        get: { assignment.area },
+                                        set: { newValue in
+                                            // Handle swap logic here
+                                            if let swapAssignment = currentSchedule?.assignments.first(where: { $0.area == newValue }) {
+                                                let temp = assignment.area
+                                                assignment.area = swapAssignment.area
+                                                swapAssignment.area = temp
+                                            } else {
+                                                assignment.area = newValue
+                                            }
+                                            
+                                            do {
+                                                try modelContext.save()
+                                            } catch {
+                                                print("Error saving assignments: \(error)")
+                                            }
+                                        }
+                                    )) {
+                                        ForEach(areas, id: \.self) { area in
+                                            Text(area)
+                                        }
+                                    }
+                                    .labelsHidden()
+                                    .frame(minWidth: 0, maxHeight: 30) // control height
+                                    .clipped()
+                                    .padding(0)
+                                }
+                                else {
+                                    Text(assignment.area)
+                                }
+                            }
+                            Text(assignment.shiftType.description)
+                                .font(.caption)
+                            
+                        }
+                    }
+                    
+                }
+                .listRowSpacing(10)
+            }
+            
+            
+            
+        }
+    }
+}
