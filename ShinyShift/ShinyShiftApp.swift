6e987809de5d69a2f494008f245d11904060d8da
mutakin-san <mutakin.email@gmail.com>
2025-05-09 14:15:34 +0700

    Renaming project file and folder

--- ShinyShift/ShinyShiftApp.swift
+++ ShinyShift/ShinyShiftApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import SwiftData
 
 @main
-struct CleanPlotApp: App {
+struct ShinyShiftApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Schedule.self
